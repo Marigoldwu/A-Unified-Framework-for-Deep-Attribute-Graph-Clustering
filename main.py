10c3e8bb76be06338b444f433d8a928c6d9e5168
Marigold <1376579889@qq.com>
2023-05-08 11:30:07 +0800

    optimize code structure

--- main.py
+++ main.py
@@ -7,93 +7,68 @@
 @WeChat Account: Marigold
 """
 import torch
-import argparse
 import importlib
 import numpy as np
-from dataset import dataset_info
-from utils import load_data, logger, time_manager, path_manager, calculator, plot, rand
 
+from utils.options import parser
+from dataset import dataset_info
+from utils import load_data, logger, time_manager, path_manager, calculator, plot, rand, data_processor
+from utils.record import record_metrics
 
 if __name__ == "__main__":
-    # Sometimes when faced with many experimental result files, we don't remember which experiment it was.
-    # So, join me in forming the habit of recording the key points of the experiment!
-    # Describe the key points of this experiment in brief words. You can record whatever you want to!
     rand.setup_seed(325)
-    description = "SDCN模型在ACM数据集上进行10次实验"
-    root_path = "/content/drive/MyDrive/DeepGraphClustering"
-    """
-    long model_name for copy conveniently:
-    model:
-        --model_name DAEGC --dataset_name acm
-        --model_name SDCN --dataset_name acm
-        --model_name AGCN --dataset_name acm
-        --model_name EFRDGC --dataset_name acm
-    pretrain_model:
-        pretrain_ae:
-            --is_pretrain True --model_name pretrain_ae_for_sdcn --dataset_name acm
-            --is_pretrain True --model_name pretrain_ae_for_agcn --dataset_name acm
-            --is_pretrain True --model_name pretrain_ae_for_efrdgc --dataset_name acm
-        pretrain_gat:
-            --is_pretrain True --model_name pretrain_gat_for_daegc --dataset_name acm
-        pretrain_gae:
-            --is_pretrain True --model_name pretrain_gae_for_efrdgc --dataset_name acm
-    """
-    parser = argparse.ArgumentParser(description='Unified Code Framework of Deep Graph Clustering')
-    parser.add_argument('--is_pretrain', type=bool, default=False)
-    parser.add_argument('--model_name', type=str, default="SDCN")
-    parser.add_argument('--dataset_name', type=str, default="acm")
-    parser.add_argument("--k", type=int, default=None)
-    parser.add_argument("--t", type=int, default=2)
-    parser.add_argument("--loops", type=int, default=1)
-    parser.add_argument("--is_change_root_path", type=bool, default=False)
-    parser.add_argument("--plot_clustering_tsne", type=bool, default=False)
-    parser.add_argument("--plot_embedding_heatmap", type=bool, default=False)
     args = parser.parse_args()
+
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
-    args = dataset_info.get_dataset_info(args.dataset_name, args)
-    args = path_manager.get_abs_path(args, root_path)
+    args = dataset_info.get_dataset_info(args)
+    args = path_manager.get_path(args)
 
     # Configuration of logger and timer module.
     # The logger print the training log to the specified file and the timer record training's time assuming.
-    current_time = time_manager.get_format_time()
-    logger_file_name = current_time + ".log"
+    logger_file_name = f"{time_manager.get_format_time()}.log"
     logger = logger.MyLogger(args.model_name, log_file_path=args.log_save_path + logger_file_name)
-    logger.info("The key points of this experiment: " + description)
+    logger.info("The key points of this experiment: " + args.desc)
     logger.info(str(args))
 
     timer = time_manager.MyTime()
 
     # Load data, including features, label, adjacency matrix.
-    # Note: the store format of data is numpy.
-    feature, label, adj = load_data.load_graph_data(args.dataset_path, args.dataset_name)
-    adj = adj + np.eye(adj.shape[0])
+    # If the dataset is not graph dataset, use construct_graph to get KNN graph.
+    # Note: the store format of data is numpy, and the adj has no self-loop.
+    if args.k is None:
+        feature, label, adj = load_data.load_graph_data(args.dataset_path, args.dataset_name)
+    else:
+        feature, label = load_data.load_data(args.dataset_path, args.dataset_name)
+        metric_dict = {"usps": "heat", "hhar": "cosine", "reut": "cosine"}
+        adj = data_processor.construct_graph(feature, args.k, metric_dict[args.dataset_name])
 
     # Auto import the training module of the model you specified.
     model_train = importlib.import_module(f"model.{args.model_name}.train")
     train = getattr(model_train, "train")
 
     # Training
-    acc, nmi, ari, f1 = [], [], [], []
-    # repeat args.loops times
+    acc_list, nmi_list, ari_list, f1_list = [], [], [], []
+    # repeat args.loops rounds
     for i in range(args.loops):
-        logger.info("Training loop No.{}".format(i + 1))
+        logger.info(f"==================Training loop No.{i+1}==================")
         timer.start()
         # call the training function of your specified model
         embedding, max_acc_corresponding_metrics = train(args, feature, label, adj, logger)
-        # record the max value of each loop
-        acc.append(max_acc_corresponding_metrics[0])
-        nmi.append(max_acc_corresponding_metrics[1])
-        ari.append(max_acc_corresponding_metrics[2])
-        f1.append(max_acc_corresponding_metrics[3])
-        # draw the clustering image or embedding heatmap
-        if args.plot_clustering_tsne:
-            plot.plot_clustering_tsne(args, embedding, label, logger)
-        if args.plot_embedding_heatmap:
-            plot.plot_embedding_heatmap(args, embedding, logger)
+
         seconds, minutes = timer.stop()
         logger.info("Time consuming: {}s or {}m".format(seconds, minutes))
 
+        # record the max value of each loop
+        acc_list, nmi_list, ari_list, f1_list = record_metrics(acc_list, nmi_list, ari_list, f1_list,
+                                                               max_acc_corresponding_metrics)
+        # draw the clustering image or embedding heatmap
+        if args.plot_clustering_tsne:
+            plot.plot_clustering_tsne(args, embedding, label, logger, desc=f"{i}", title=None, axis_show=False)
+        if args.plot_embedding_heatmap:
+            plot.plot_embedding_heatmap(args, embedding, logger, desc=f"{i}", title=None,
+                                        axis_show=False, color_bar_show=True)
+
     logger.info("Total loops: {}".format(args.loops))
     logger.info("Mean value:")
-    logger.info(calculator.cal_mean_std(acc, nmi, ari, f1))
+    logger.info(calculator.cal_mean_std(acc_list, nmi_list, ari_list, f1_list))
     logger.info("Training over! Punch out!")
