77551f09a397a8c8765ef0cc833835657d3754d8
Marigold <1376579889@qq.com>
2023-05-24 15:49:02 +0800

    fix GAE module

--- module/GAE.py
+++ module/GAE.py
@@ -17,10 +17,10 @@ class GAE(nn.Module):
     def __init__(self, input_dim, hidden_size, embedding_dim):
         super(GAE, self).__init__()
         self.gcn1 = GCN(input_dim, hidden_size)
-        self.gcn2 = GCN(hidden_size, embedding_dim)
+        self.gcn2 = GCN(hidden_size, embedding_dim, activeType='no')
 
     def forward(self, x, adj):
-        h1 = F.relu(self.gcn1(x, adj))
+        h1 = self.gcn1(x, adj)
         h2 = self.gcn2(h1, adj)
         embedding = F.normalize(h2, p=2, dim=1)
         A_pred = dot_product_decode(embedding)
